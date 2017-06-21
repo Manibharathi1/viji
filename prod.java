5d697ee2fa54c9236675c5861f802637053b1c2b
Manibharathi1 <manibharathisathiyaseelan@gmail.com>
2017-06-21 14:49:03 +0530

    prod.java

---
+++ prod.java
@@ -0,0 +1,27 @@
+import java.io*;
+import java.util.*;
+public class prod
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[];
+for(int i=0;i<a.length;i++)
+{
+a[i]=Integer.parseInt();
+}
+for(int i=0;i<a.length;i++)
+{
+product=1;
+for(int j=0;j<a.length;j++)
+{
+if(i!=j)
+{
+product=product*b[j];
+}
+}
+System.out.println(product+" ");
+}
+}
+}
